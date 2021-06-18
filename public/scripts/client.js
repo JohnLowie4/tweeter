d870b1d1d8ced5dbf3572c06613b01c25960b8c0
John <jzlowie@gmail.com>
2021-06-18 15:43:00 -0600

    Cleaned up code, deleted test cases and unused code and documentations

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,36 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1623708961391
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd"
-//     },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ];
-
-/**
- * 
- */
 $(document).ready(function () {
   
   /**
